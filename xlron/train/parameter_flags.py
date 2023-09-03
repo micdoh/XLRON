049367ecba5603a8aeefabf4f07fee77ed573ccb
micdoh <mic.d.doh@gmail.com>
2023-09-03 18:31:55 +0100

    Use abseil flags in separate training script

---
+++ xlron/train/parameter_flags.py
@@ -0,0 +1,45 @@
+from absl import flags
+
+# N.B. Use can pass the flag --flagfile=PATH_TO_FLAGFILE to add flags without typing them out
+
+# Training hyperparameters
+flags.DEFINE_float("LR", 5e4, "Learning rate")
+flags.DEFINE_integer("NUM_ENVS", 1, "Number of environments")
+flags.DEFINE_integer("NUM_STEPS", 150, "Number of steps per environment")
+flags.DEFINE_float("TOTAL_TIMESTEPS", 1e6, "Total number of timesteps")
+flags.DEFINE_integer("UPDATE_EPOCHS", 1, "Number of epochs per update")
+flags.DEFINE_integer("NUM_MINIBATCHES", 1, "Number of minibatches per update")
+flags.DEFINE_float("GAMMA", 0.99, "Discount factor")
+flags.DEFINE_float("GAE_LAMBDA", 0.95, "GAE lambda parameter")
+flags.DEFINE_float("CLIP_EPS", 0.2, "PPO clipping parameter")
+flags.DEFINE_float("ENT_COEF", 0.01, "Entropy coefficient")
+flags.DEFINE_float("VF_COEF", 0.5, "Value function coefficient")
+flags.DEFINE_float("MAX_GRAD_NORM", 0.5, "Maximum gradient norm")
+flags.DEFINE_string("ACTIVATION", "tanh", "Activation function")
+flags.DEFINE_boolean("ANNEAL_LR", True, "Anneal learning rate")
+flags.DEFINE_integer("SEED", 42, "Random seed")
+# Additional training parameters
+flags.DEFINE_boolean("USE_PMAP", False, "Use pmap")
+flags.DEFINE_boolean("WANDB", False, "Use wandb")
+flags.DEFINE_boolean("SAVE_MODEL", False, "Save model")
+flags.DEFINE_integer("NUM_DEVICES", None, "Number of devices to emulate")
+flags.DEFINE_boolean("DEBUG", False, "Debug mode")
+flags.DEFINE_boolean("ORDERED", True, "Order print statements when debugging "
+                                      "(must be false if using pmap)")
+flags.DEFINE_string("MODEL_PATH", ".", "Path to save/load model")
+# Environment parameters
+flags.DEFINE_string("env_type", "vone", "Environment type")
+flags.DEFINE_integer("load", 100, "Load")
+flags.DEFINE_integer("k", 5, "Number of paths")
+flags.DEFINE_string("topology_name", "4node", "Topology name")
+flags.DEFINE_integer("link_resources", 4, "Number of link resources")
+flags.DEFINE_float("max_requests", 4, "Maximum number of requests in an episode")
+flags.DEFINE_float("max_timesteps", 30, "Maximum number of timesteps in an episode")
+flags.DEFINE_integer("min_slots", 1, "Minimum number of slots")
+flags.DEFINE_integer("max_slots", 1, "Maximum number of slots")
+# VONE-specific environment parameters
+flags.DEFINE_integer("mean_service_holding_time", 10, "Mean service holding time")
+flags.DEFINE_integer("node_resources", 4, "Number of node resources")
+flags.DEFINE_list("virtual_topologies", "3_ring", "Virtual topologies")
+flags.DEFINE_integer("min_node_resources", 1, "Minimum number of node resources")
+flags.DEFINE_integer("max_node_resources", 1, "Maximum number of node resources")
---
+++ xlron/train/train.py
@@ -0,0 +1,56 @@
+import wandb
+import os
+import jax
+import matplotlib.pyplot as plt
+import orbax.checkpoint
+from flax.training import orbax_utils
+from absl import app, flags
+from xlron.environments.env_funcs import *
+from xlron.train.ppo_vone import make_train
+import xlron.train.parameter_flags
+
+
+FLAGS = flags.FLAGS
+
+
+def main(argv):
+    # TODO - Add wandb
+    # Set the number of (emulated) host devices
+    num_devices = FLAGS.NUM_DEVICES if FLAGS.NUM_DEVICES is not None else jax.local_device_count()
+    os.environ['XLA_FLAGS'] = f"--xla_force_host_platform_device_count={num_devices}"
+
+    # Print every flag and its name
+    if FLAGS.DEBUG:
+        print('non-flag arguments:', argv)
+    for name in FLAGS:
+        print(name, FLAGS[name].value)
+
+    rng = jax.random.PRNGKey(FLAGS.SEED)
+
+    with TimeIt(tag='COMPILATION'):
+        if FLAGS.USE_PMAP:
+            FLAGS.ORDERED = False
+            rng = jax.random.split(rng, num_devices)
+            train_jit = jax.pmap(make_train(FLAGS), devices=jax.devices()).lower(rng).compile()
+        else:
+            train_jit = jax.jit(make_train(FLAGS)).lower(rng).compile()
+
+    with TimeIt(tag='EXECUTION', frames=FLAGS.TOTAL_TIMESTEPS):
+        out = train_jit(rng)
+
+    # Save model params
+    if FLAGS.SAVE_MODEL:
+        state = out["runner_state"][0]
+        save_data = {"model": state, "config": FLAGS}
+        orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
+        save_args = orbax_utils.save_args_from_target(save_data)
+        orbax_checkpointer.save(FLAGS.MODEL_PATH, save_data, save_args=save_args)
+
+    plt.plot(out["metrics"]["returned_episode_returns"].mean(-1).reshape(-1))
+    plt.xlabel("Update Step")
+    plt.ylabel("Return")
+    plt.show()
+
+
+if __name__ == "__main__":
+    app.run(main)
