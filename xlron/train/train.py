10dd2e4ed564f24ce1ca8c9cb1b7954f19fbf76f
micdoh <mic.d.doh@gmail.com>
2023-09-08 16:43:34 +0100

    default device flag

--- xlron/train/train.py
+++ xlron/train/train.py
@@ -21,7 +21,8 @@ def main(argv):
     os.environ['XLA_FLAGS'] = f"--xla_force_host_platform_device_count={num_devices}"
 
     # Set the default device
-    jax.config.update("jax_default_device", jax.devices()[FLAGS.DEFAULT_DEVICE])
+    print(jax.devices())
+    jax.default_device = jax.devices()[FLAGS.DEFAULT_DEVICE]
 
     # Print every flag and its name
     if FLAGS.DEBUG:
