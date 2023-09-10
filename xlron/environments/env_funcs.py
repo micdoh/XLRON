Corrected node masking and request generation

xlron/environments/env_funcs.py
@@ -340,8 +340,7 @@ def generate_vone_request(key: chex.PRNGKey, state: EnvState, params: EnvParams)
     The remaining elements of the  pattern show the virtual topology pattern, i.e. the connectivity of the virtual topology.
     """
     # TODO - update this to be bitrate requests rather than slots
-    # Define the four possible patterns for the first row
-    shape = state.request_array.shape[1]
+    shape = params.max_edges*2+1  # shape of request array
     key_topology, key_node, key_slot, key_times = jax.random.split(key, 4)
     # Randomly select topology, node resources, slot resources
     pattern = jax.random.choice(key_topology, state.virtual_topology_patterns)
@@ -353,7 +352,16 @@ def generate_vone_request(key: chex.PRNGKey, state: EnvState, params: EnvParams)
     mask = jnp.tile(jnp.array([0, 1]), (shape+1) // 2)[:shape]
     # Vectorized conditional replacement using mask
     first_row = jnp.where(mask, selected_slot_values, selected_node_values)
+    # Make sure node requests are consistent for same virtual nodes
+    first_row = jax.lax.fori_loop(
+        2,  # Lowest node index in virtual topology requests is 2
+        shape,  # Highest possible node index in virtual topology requests is shape-1
+        lambda i, x: jnp.where(topology_pattern == topology_pattern[i], selected_node_values[i], x),
+        first_row
+    )
+    # Mask out unused part of request array
     first_row = jnp.where(topology_pattern == 0, 0, first_row)
+    # Set times
     arrival_time, holding_time = generate_arrival_holding_times(key, params)
     state = state.replace(
         holding_time=holding_time,
@@ -363,8 +371,8 @@ def generate_vone_request(key: chex.PRNGKey, state: EnvState, params: EnvParams)
         action_history=init_action_history(params),
         total_requests=state.total_requests + 1
     )
-    state = remove_expired_node_requests(state) if params.consecutive_loading else state
-    state = remove_expired_slot_requests(state) if params.consecutive_loading else state
+    state = remove_expired_node_requests(state) if not params.consecutive_loading else state
+    state = remove_expired_slot_requests(state) if not params.consecutive_loading else state
     return state
 
 
@@ -389,7 +397,7 @@ def generate_rsa_request(key: chex.PRNGKey, state: EnvState, params: EnvParams) 
         request_array=jnp.stack((source, slots, dest)),
         total_requests=state.total_requests + 1
     )
-    state = remove_expired_slot_requests(state) if params.consecutive_loading else state
+    state = remove_expired_slot_requests(state) if not params.consecutive_loading else state
     return state
 
 
@@ -564,7 +572,6 @@ def check_node_capacities(capacity_array):
 def check_no_spectrum_reuse(link_slot_array):
     """slot-=1 when used, should be zero when occupied, so check if any < -1 in slot array
     Return False if check passed, True if check failed"""
-    #jax.debug.print("check_no_spectrum_reuse {}", link_slot_array, ordered=True)
     return jnp.any(link_slot_array < -1)
 
 
@@ -900,8 +907,8 @@ def mask_nodes(state: EnvState, num_nodes: chex.Scalar) -> EnvState:
     request = jax.lax.dynamic_slice_in_dim(full_request, (remaining_actions - 1) * 2, 3)
     node_request_s = jax.lax.dynamic_slice_in_dim(request, 2, 1)
     node_request_d = jax.lax.dynamic_slice_in_dim(request, 0, 1)
-    prev_action = jax.lax.dynamic_slice_in_dim(state.action_history, (remaining_actions - 1) * 2, 3)
-    prev_source = jax.lax.dynamic_slice_in_dim(prev_action, 2, 1)
+    prev_action = jax.lax.dynamic_slice_in_dim(state.action_history, (remaining_actions) * 2, 3)
+    prev_dest = jax.lax.dynamic_slice_in_dim(prev_action, 0, 1)
     node_indices = jnp.arange(0, num_nodes)
     # Get requested indices from request array virtual topology
     requested_indices = jax.lax.dynamic_slice_in_dim(virtual_topology, (remaining_actions-1)*2, 3)
@@ -923,7 +930,7 @@ def mask_nodes(state: EnvState, num_nodes: chex.Scalar) -> EnvState:
                 jnp.zeros(num_nodes)
             ),
             lambda x: jnp.where(
-                node_indices == prev_source,
+                node_indices == prev_dest,
                 x,
                 jnp.zeros(num_nodes)
             ),
@@ -957,15 +964,15 @@ def mask_nodes(state: EnvState, num_nodes: chex.Scalar) -> EnvState:
         update_slice = lambda j, x: jax.lax.dynamic_update_slice_in_dim(x, jnp.array([0.]), j, axis=0)
         val = jax.lax.cond(
             i % 2 == 0,
-            lambda x: update_slice(*x),  # i is node request index
-            lambda x: update_slice(x[0]+1, x[1]),  # i is slot request index (so add 1 to get next node)
-            (i, val),
+            lambda x: update_slice(x[0][i], x[1]),  # i is node request index
+            lambda x: update_slice(x[0][i+1], x[1]),  # i is slot request index (so add 1 to get next node)
+            (state.action_history, val),
         )
         return val
 
     state = state.replace(
         node_mask_d=jax.lax.fori_loop(
-            (remaining_actions)*2,
+            remaining_actions*2,
             state.action_history.shape[0]-1,
             mask_previous_selections,
             state.node_mask_d

xlron/environments/env_test.py
@@ -90,10 +90,24 @@ class GenerateVoneRequestTest(parameterized.TestCase):
 
     @chex.all_variants()
     @parameterized.named_parameters(
-        ('case_base', jnp.array([[1,1,1,1,1,1,1], [2,1,3,1,4,1,2]])),
+        ('case_1', np.array([1, 2], dtype=np.uint32), jnp.array([[1,1,1,1,1,1,1], [2,1,3,1,4,1,2]])),
+        ('case_2', np.array([2, 1], dtype=np.uint32), jnp.array([[1, 1, 1, 1, 1, 1, 1], [2, 1, 3, 1, 4, 1, 2]])),
+        ('case_3', np.array([5, 5], dtype=np.uint32), jnp.array([[1, 1, 1, 1, 1, 1, 1], [2, 1, 3, 1, 4, 1, 2]])),
     )
-    def test_generate_vone_request(self, expected):
-        key = np.array([1, 2], dtype=np.uint32)
+    def test_generate_vone_request(self, key, expected):
+        state = self.variant(generate_vone_request)(key, self.state, self.params)
+        request = state.request_array
+        chex.assert_trees_all_close(request, expected)
+
+
+    @chex.all_variants()
+    @parameterized.named_parameters(
+        ('case_1', np.array([1, 2], dtype=np.uint32), jnp.array([[1,2,2,2,2,2,1], [2,1,3,1,4,1,2]])),
+        ('case_2', np.array([2, 1], dtype=np.uint32), jnp.array([[2, 1, 2, 1, 1, 1, 2], [2, 1, 3, 1, 4, 1, 2]])),
+        ('case_3', np.array([5, 5], dtype=np.uint32), jnp.array([[2,2, 1, 2,2,2,2], [2, 1, 3, 1, 4, 1, 2]])),
+    )
+    def test_generate_vone_nsfnet(self, key, expected):
+        self.key, self.env, self.obs, self.state, self.params = vone_nsfnet_16_test_setup()
         state = self.variant(generate_vone_request)(key, self.state, self.params)
         request = state.request_array
         chex.assert_trees_all_close(request, expected)
@@ -1268,6 +1282,27 @@ class MaskNodesTest(parameterized.TestCase):
          jnp.array([1, 0, 0, 0, 0, 0, 0, 1])),  # expected
     )
     def test_mask_nodes(self, node_capacity_array, action_history, action_counter, request_array, expected):
+        self.state = self.state.replace(
+            node_capacity_array=node_capacity_array,
+            action_history=action_history,
+            action_counter=action_counter,
+            request_array=request_array
+        )
+        state = self.variant(mask_nodes, static_argnums=(1,))(self.state, self.params.num_nodes)
+        node_mask = jnp.concatenate([state.node_mask_s, state.node_mask_d], axis=0)
+        chex.assert_trees_all_close(node_mask, expected)
+
+    @chex.all_variants()
+    @parameterized.named_parameters(
+        ("case_third_move_big_requests",
+         jnp.array([4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]),  # node_capacity_array
+         jnp.array([-1, -1, -1, -1, 7, 15, 1]),  # action_history
+         jnp.array([3, 3, 2]),  # action_counter
+         jnp.array([[2, 2, 1, 4, 2, 4, 2], [2, 1, 3, 1, 4, 1, 2]]),  # request_array
+         jnp.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,   1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1])),  # expected
+    )
+    def test_mask_nodes_nsfnet(self, node_capacity_array, action_history, action_counter, request_array, expected):
+        self.key, self.env, self.obs, self.state, self.params = vone_nsfnet_16_test_setup()
         self.state = self.state.replace(
             node_capacity_array=node_capacity_array,
             action_history=action_history,