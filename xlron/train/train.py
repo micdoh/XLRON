833de496648406001c9d3d67eefe36412d6828cd
micdoh <mic.d.doh@gmail.com>
2024-05-16 14:57:51 +0100

    Update training script with episode end metrics

--- xlron/train/train.py
+++ xlron/train/train.py
@@ -19,6 +19,7 @@ FLAGS = flags.FLAGS
 def main(argv):
 
     # Set visible devices
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.VISIBLE_DEVICES
     print(f"CUDA_VISIBLE_DEVICES={os.environ['CUDA_VISIBLE_DEVICES']}")
     # Allow time for environment variable to take effect
@@ -30,6 +31,7 @@ def main(argv):
     jax.config.update("jax_disable_jit", FLAGS.DISABLE_JIT)
     jax.config.update("jax_enable_x64", FLAGS.ENABLE_X64)
     print(f"Available devices: {jax.devices()}")
+    print(f"Local devices: {jax.local_devices()}")
     num_devices = len(jax.devices())  # or len(FLAGS.VISIBLE_DEVICES.split(","))
     import jax.numpy as jnp
     import orbax.checkpoint
@@ -73,12 +75,17 @@ def main(argv):
         )
         wandb.config.update(FLAGS)
         run.name = experiment_name
+        wandb.define_metric('episode_count')
         wandb.define_metric("update_step")
         wandb.define_metric("lengths", step_metric="update_step")
         wandb.define_metric("returns", step_metric="update_step")
         wandb.define_metric("cum_returns", step_metric="update_step")
         wandb.define_metric("episode_lengths", step_metric="update_step")
         wandb.define_metric("episode_returns", step_metric="update_step")
+        wandb.define_metric("episode_accepted_services", step_metric="episode_count")
+        wandb.define_metric("episode_accepted_services_std", step_metric="episode_count")
+        wandb.define_metric("episode_accepted_bitrate", step_metric="episode_count")
+        wandb.define_metric("episode_accepted_bitrate_std", step_metric="episode_count")
 
     # Print every flag and its name
     if FLAGS.DEBUG:
@@ -91,7 +98,7 @@ def main(argv):
 
     make_func = make_train if not (FLAGS.EVAL_HEURISTIC or FLAGS.EVAL_MODEL) else make_eval
 
-    if FLAGS.EVAL_MODEL:
+    if FLAGS.LOAD_MODEL or FLAGS.EVAL_MODEL:
         orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
         model = orbax_checkpointer.restore(pathlib.Path(FLAGS.MODEL_PATH))
         FLAGS.__setattr__("model", model)
@@ -135,7 +142,7 @@ def main(argv):
             i += 1
         print(f"Saving model to {model_path}")
         orbax_checkpointer.save(model_path, save_data, save_args=save_args)
-        # TODO - Upload model to wandb
+        # Upload model to wandb
         if FLAGS.WANDB:
             print((model_path / "*").absolute())
             wandb.save(str((model_path / "*").absolute()), base_path=str(model_path.parent))
@@ -159,10 +166,6 @@ def main(argv):
         accepted_bitrate_mean = out["metrics"]["accepted_bitrate"].mean(-1).mean(0).reshape(-1)
         accepted_bitrate_std = out["metrics"]["accepted_bitrate"].mean(-1).std(0).reshape(-1)
         done = out["metrics"]["done"].mean(-1).mean(0).reshape(-1)
-        # episode_accepted_services_mean = out["metrics"]["episode_accepted_services"].mean(-1).mean(0).reshape(-1)
-        # episode_accepted_services_std = out["metrics"]["episode_accepted_services"].mean(-1).std(0).reshape(-1)
-        # episode_accepted_bitrate_mean = out["metrics"]["episode_accepted_bitrate"].mean(-1).mean(0).reshape(-1)
-        # episode_accepted_bitrate_std = out["metrics"]["episode_accepted_bitrate"].mean(-1).std(0).reshape(-1)
     else:
         # N.B. This is the same as the above code, but without the mean on the seed dimension
         # This means the results are still per update step
@@ -181,25 +184,19 @@ def main(argv):
         accepted_bitrate_mean = out["metrics"]["accepted_bitrate"].mean(-1).reshape(-1)
         accepted_bitrate_std = out["metrics"]["accepted_bitrate"].std(-1).reshape(-1)
         done = out["metrics"]["done"].mean(-1).reshape(-1)
-        # episode_accepted_services_mean = out["metrics"]["episode_accepted_services"].mean(-1).reshape(-1)
-        # episode_accepted_services_std = out["metrics"]["episode_accepted_services"].std(-1).reshape(-1)
-        # episode_accepted_bitrate_mean = out["metrics"]["episode_accepted_bitrate"].mean(-1).reshape(-1)
-        # episode_accepted_bitrate_std = out["metrics"]["episode_accepted_bitrate"].std(-1).reshape(-1)
 
     episode_ends = np.where(done == 1)[0]
     # shift end indices by -1
     episode_ends = episode_ends - 1
     # get values of accepted services and bitrate at episode ends
     episode_end_accepted_services = accepted_services_mean[episode_ends]
+    episode_end_accepted_services_std = accepted_services_std[episode_ends]
     episode_end_accepted_bitrate = accepted_bitrate_mean[episode_ends]
+    episode_end_accepted_bitrate_std = accepted_bitrate_std[episode_ends]
     # Do box and whisker plot of accepted services and bitrate at episode ends
     plt.boxplot(episode_end_accepted_services)
     plt.title(experiment_name)
     plt.show()
-
-    # This is valid for the case of +1 success -1 fail per request
-    service_blocking_probability = 1 - ((episode_returns_mean + FLAGS.max_requests) / (2*FLAGS.max_requests))
-    service_blocking_probability_std = episode_returns_std / (2*FLAGS.max_requests)
 
     service_blocking_probability = 1 - ((cum_returns_mean + lengths_mean) / (2*lengths_mean))
     service_blocking_probability_std = returns_std / (2*lengths_mean)
@@ -262,11 +259,16 @@ def main(argv):
         accepted_services_std = accepted_services_std[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
         accepted_bitrate_mean = accepted_bitrate_mean[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
         accepted_bitrate_std = accepted_bitrate_std[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
-        done = done[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
-        # episode_accepted_services_mean = episode_accepted_services_mean[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
-        # episode_accepted_services_std = episode_accepted_services_std[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
-        # episode_accepted_bitrate_mean = episode_accepted_bitrate_mean[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
-        # episode_accepted_bitrate_std = episode_accepted_bitrate_std[chop:].reshape(-1, FLAGS.DOWNSAMPLE_FACTOR).mean(axis=1)
+
+        for i in range(len(episode_end_accepted_services)):
+            log_dict = {
+                "episode_count": i,
+                "episode_end_accepted_services": episode_end_accepted_services[i],
+                "episode_end_accepted_services_std": episode_end_accepted_services_std[i],
+                "episode_end_accepted_bitrate": episode_end_accepted_bitrate[i],
+                "episode_end_accepted_bitrate_std": episode_end_accepted_bitrate_std[i],
+            }
+            wandb.log(log_dict)
 
         for i in range(len(episode_returns_mean)):
             # Log the data
@@ -288,10 +290,6 @@ def main(argv):
                 "accepted_services_std": accepted_services_std[i],
                 "accepted_bitrate_mean": accepted_bitrate_mean[i],
                 "accepted_bitrate_std": accepted_bitrate_std[i],
-                # "episode_accepted_services_mean": episode_accepted_services_mean[i],
-                # "episode_accepted_services_std": episode_accepted_services_std[i],
-                # "episode_accepted_bitrate_mean": episode_accepted_bitrate_mean[i],
-                # "episode_accepted_bitrate_std": episode_accepted_bitrate_std[i],
             }
             wandb.log(log_dict)
 
