b79f3a5d3caecda5530921b50b67cade8a833209
myianwang <myianwang@gmail.com>
2024-11-28 15:48:55 +0000

    Add bash file to run multiband env

---
+++ run_multiband.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run the training script with specified parameters
+python /Users/ianwang/PycharmProjects/XLRON/xlron/train/train.py \
+  --env_type=multibandrsa \
+  --continuous_operation \
+  --load=400 \
+  --k=5 \
+  --topology_name=nsfnet_deeprmsa_directed \
+  --link_resources=100 \
+  --max_requests=1e3 \
+  --max_timesteps=1e3 \
+  --mean_service_holding_time=25 \
+  --continuous_operation \
+  --EVAL_HEURISTIC \
+  --path_heuristic=ksp_ff \
+  --NUM_ENVS=10 \
+  --TOTAL_TIMESTEPS=100000 \
+  --PLOTTING \
+  --slot_size=50 \
+  --bandgap_start=2500 \
+  --bandgap=100 \
+  --WANDB
